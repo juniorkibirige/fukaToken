3893da47834666c52121cb0e998b046a56f13f26
Kibirige Junior Lawrence <aanga2@gmail.com>
2020-09-24 11:00:44 +0300

    Added Auth for login and jwt_secret

---
+++ auth/controllers/auth.controller.js
@@ -0,0 +1,36 @@
+const JWT_S = process.env.JWT_SECRET,
+    jwt = require('jsonwebtoken')
+const crypto = require('crypto')
+const uuid = require('uuid')
+
+var refreshTokens = {}
+
+exports.login = (req, res) => {
+    try {
+        let refreshId = req.body.userId + JWT_S;
+        let salt = crypto.randomBytes(16).toString('base64')
+        let hash = crypto.createHmac('sha512', salt).update(refreshId).digest('base64')
+        req.body.refreshKey = salt
+        let token = jwt.sign(req.body, JWT_S)
+        let b = Buffer.from(hash, 'utf8');
+        let refresh_token = b.toString('base64')
+        res.status(201).send({ success: true, accessId: req.body.userId, accessToken: token, refreshToken: refresh_token })
+
+    } catch (err) {
+        res.status(500).send({ error: err })
+    }
+}
+
+exports.refresh_token = (req, res) => {
+    try {
+        req.body = req.jwt;
+        let token = jwt.sign(req.body, jwtSecret);
+        res.status(201).send({ id: token });
+    } catch (err) {
+        res.status(500).send({ errors: err });
+    }
+}
+
+exports.getDetails = (req, res) => {
+
+}
---
+++ auth/middlewares/verify.user.middleware.js
@@ -0,0 +1,70 @@
+const UserModel = require('../../users/models/users.model')
+const crypto = require('crypto')
+
+exports.hasAuthValidFields = (req, res, next) => {
+    let errors = Array()
+    if (req.body) {
+        !req.body.email && !req.body.phoneNumber ? errors.push('Missing email/phone field!') : null
+        !req.body.email ? !req.body.phoneNumber ? errors.push('Missing email/phone field!') : null : null
+        !req.body.passwd ? errors.push('Missing password field!') : null
+        return errors.length ? res.status(400).send({ emsg: errors.join('|') }) :
+            next()
+    } else {
+        return res.status(400).send({ emsg: 'Please fill in all fields!' })
+    }
+}
+
+exports.isPasswordAndUserMatch = (req, res, next) => {
+    // req.body = req.query
+    if (req.body.email !== undefined) {
+        UserModel.findByEmail(req.body.email)
+            .then(user => {
+                if (!user[0]) {
+                    res.status(404).send({ success: false, errors: ['User doesn\'t exist!'] })
+                } else {
+                    let concat = Buffer.from(process.env.HASH_CONCAT, 'utf-8').toString('base64')
+                    let passwordFields = user[0].passwd.split(concat)
+                    let salt = passwordFields[0]
+                    let hash = crypto.createHmac('sha512', salt).update(req.body.passwd).digest('base64')
+                    if (hash === passwordFields[1]) {
+                        req.body = {
+                            userId: user[0]._id,
+                            email: user[0].email,
+                            permissionLevel: user[0].permissionLevel,
+                            provider: 'email',
+                            firstName: user[0].firstName,
+                            lastName: user[0].lastName
+                        }
+                        return next()
+                    } else {
+                        return res.status(400).send({ errors: ['Invalid password'] })
+                    }
+                }
+            })
+    } else if(req.body.phoneNumber !== undefined) {
+        UserModel.findByPhone(req.body.phoneNumber)
+        .then(user => {
+            if (!user[0]) {
+                res.status(404).send({ success: false, errors: ['User doesn\'t exist!'] })
+            } else {
+                let concat = Buffer.from(process.env.HASH_CONCAT, 'utf-8').toString('base64')
+                let passwordFields = user[0].passwd.split(concat)
+                let salt = passwordFields[0]
+                let hash = crypto.createHmac('sha512', salt).update(req.body.passwd).digest('base64')
+                if (hash === passwordFields[1]) {
+                    req.body = {
+                        userId: user[0]._id,
+                        email: user[0].email,
+                        permissionLevel: user[0].permissionLevel,
+                        provider: 'phone',
+                        firstName: user[0].firstName,
+                        lastName: user[0].lastName
+                    }
+                    return next()
+                } else {
+                    return res.status(400).send({ errors: ['Invalid password'] })
+                }
+            }
+        })
+    }
+}
